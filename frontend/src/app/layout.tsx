3baef9812a58ac4ddd654078f2d27be651db9f90
Jeovana_rl_Valente <jeovanalopesvalente@gmail.com>
2026-06-06 01:49:34 -0300

    fixed: correções

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -40,6 +40,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="pt-BR">
       <head>
         <link
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+          rel="stylesheet"
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+          crossOrigin=""
+        />
+        <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           rel="stylesheet"
         />
