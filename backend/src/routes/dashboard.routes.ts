a3c9ac4acf449db952c55a7ab21de68689dd54dd
Guilherme Matos <guilherme.c.matos@aluno.senai.br>
2026-06-11 17:08:14 -0300

    fixed: deletado backend

--- backend/src/routes/dashboard.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { dashboardController } from "../controllers/dashboard.controller";
-import { authenticate } from "../middlewares/auth.middleware";
-
-const router = Router();
-
-router.get("/", authenticate, (req, res) => dashboardController.index(req, res));
-
-export default router;
--- backend/src/utils/errors.ts
+++
@@ -1,5 +0,0 @@
-import { Response } from "express";
-
-export function sendError(res: Response, status: number, message: string) {
-  return res.status(status).json({ error: message });
-}
